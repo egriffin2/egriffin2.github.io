f5a7b1b8b839f05ccaae8cadf475623f4752656f
Jodi Griffin <emily1.griffin@umontana.edu>
2016-10-19 10:05:38 -0600

    mouth is closing

--- 7.1/PacManConstructor.js
+++ 7.1/PacManConstructor.js
@@ -17,6 +17,7 @@ function PacMan(){ //defining parameters for PacMan class
   this.direction = 0; //will be up,down,right, or left
   this.timeElapsed = 0; //0 frames or seconds have elapsed at the beginning of the sketch
   this.maxTime = 240; //240 frames or 4 seconds is the longest PacMan will move
+  this.mouth = 330;
 
   // mouth size
   this.arcWidth = 50;
@@ -40,7 +41,7 @@ PacMan.prototype.drawPacMan = function(){ //create PacMan
   translate(this.posX, this.posY); //places PacMan
   fill(this.r,this.g,this.b);
   angleMode(DEGREES);
-  arc(0, 0, this.arcWidth,this.arcHeight, 25,330, PIE); //creating PacMan arc
+  arc(0, 0, this.arcWidth,this.arcHeight, 25,this.mouth, PIE); //creating PacMan arc
 
   noStroke();
   fill(0); //black eye
@@ -55,16 +56,16 @@ PacMan.prototype.move = function() {
   this.posX = this.posX + (this.speed * this.dirX);
   this.posY = this.posY + (this.speed * this.dirY);
 
-  if (this.direction == 0) {
+  if (this.direction == 0) { //direction 0 = right
     this.dirX = 1;
     this.dirY = 0;
-  } else if (this.direction == 1) {
+  } else if (this.direction == 1) { //direction 1 = left
     this.dirX = -1;
     this.dirY = 0;
-  } else if (this.direction == 2) {
+  } else if (this.direction == 2) { //direction 2 = up
     this.dirX = 0;
     this.dirY = -1;
-  } else if (this.direction == 3) {
+  } else if (this.direction == 3) { //direction 3 = down
     this.dirX = 0;
     this.dirY = 1;
   }
@@ -83,24 +84,39 @@ PacMan.prototype.move = function() {
   }
 
 
-if (this.posX > width - this.arcWidth/2 - 1) {
-  this.posX = width - this.arcWidth/2 - 1;
-  this.speed = 0;
-}
-else if (this.posY > height - this.arcHeight/2 - 1) {
-  this.posY = height - this.arcHeight/2 - 1;
-  this.speed = 0;
-}
-else if (this.posX < 0 + this.arcWidth/2 + 1) {
-  this.posX = this.arcWidth/2 + 2;
-  this.speed = 0;
-}
-else if (this.posY < 0 + this.arcHeight/2 + 1) {
-  this.posY = this.arcHeight/2 + 2;
-  this.speed = 0;
-}
-text(this.timeElapsed + " : " + this.maxTime, 50, 50);
-text(this.direction + " -- " + this.speed + " : " + this.posX + " -- " + this.speed + " : " + this.posY, 50, 60);
+  if (this.posX > width - this.arcWidth/2 - 1) {
+    this.posX = width - this.arcWidth/2 - 1;
+    this.speed = 0;
+  }
+  else if (this.posY > height - this.arcHeight/2 - 1) {
+    this.posY = height - this.arcHeight/2 - 1;
+    this.speed = 0;
+  }
+  else if (this.posX < 0 + this.arcWidth/2 + 1) {
+    this.posX = this.arcWidth/2 + 2;
+    this.speed = 0;
+  }
+  else if (this.posY < 0 + this.arcHeight/2 + 1) {
+    this.posY = this.arcHeight/2 + 2;
+    this.speed = 0;
+  }
 
+  this.mouth++;
+  if (this.mouth >=380) {
+    this.mouth = this.mouth - 1;
+  }
+
+  if (this.direction == 3) {
+    translate(width/2, height/2); //PacMan SHOULD rotate 90 degrees when moving down
+    rotate(90);
+  }
+  if (this.direction == 2) {
+    translate(width/2, height/2); //PacMan SHOULD rotate 90 degrees when moving down
+    rotate(270);
+  }
+  scale(1.5, 1.3); //?
+
+  text(this.timeElapsed + " : " + this.maxTime, 50, 50);
+  text(this.direction + " -- " + this.speed + " : " + this.posX + " -- " + this.speed + " : " + this.posY, 50, 60);
 
 }
